d7d1194ea7b9620252b3bc286e1d2716f4e74330
chieby-mwangi <chiebyrah@gmail.com>
2025-02-25 09:35:35 +0300

    staging tests

--- src/components/Tests.jsx
+++ src/components/Tests.jsx
@@ -3,7 +3,7 @@ import { TestLanguages } from "../data/test-languages-data";
 import { useNavigate } from "react-router-dom";
 
 export default function Tests() {
-   const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="gap-5">
@@ -11,7 +11,13 @@ export default function Tests() {
       <p className="font-medium text-lg">Practice various languaages</p>
       <div className="w-[1000px] h-auto grid grid-cols-3 gap-2 my-10">
         {TestLanguages.map((test) => (
-            <div key={test.index} className="flex gap-4 w-[300px] items-center bg-white hover:bg-[#ebebf3] hover:cursor-pointer py-3.5 px-4 mb-4 rounded-lg" onClick={() => {navigate(`/practice/${test.language}`)}}>
+          <div
+            key={test.index}
+            className="flex gap-4 w-[300px] items-center bg-white hover:bg-[#ebebf3] hover:cursor-pointer py-3.5 px-4 mb-4 rounded-lg"
+            onClick={() => {
+              navigate(`/practice/${test.language}`);
+            }}
+          >
             <div>{test.icon}</div>
             <p className="font-bold text-[#003066]">{test.language}</p>
           </div>
