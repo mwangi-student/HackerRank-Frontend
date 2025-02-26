1ce6fb4ce088c40be365ab34c787b69a3d2b75c7
Anne Muriuki <muriukianne05@gmail.com>
2025-02-26 21:46:12 +0300

    added contexts

--- src/Contexts/AssessmentInvite.jsx
+++ src/Contexts/AssessmentInvite.jsx
@@ -12,7 +12,7 @@ export const AssessmentInviteProvider = ({ children }) => {
     // Fetch assessment invites
     useEffect(() => {
         if (authToken) {
-            axios.get("http://127.0.0.1:5000/assessment-invites", {
+            axios.get("http://127.0.0.1:5000/assessment-invite", {
                 headers: { Authorization: `Bearer ${authToken}` }
             })
             .then((response) => setAssessmentInvites(response.data))
